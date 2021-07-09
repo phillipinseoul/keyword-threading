ea0c2ff5d90f7711ee8cdd2428ce754107a0181c
Yuseung Lee <phillip9807@gmail.com>
2021-07-09 16:07:06 +0900

    ADD: main function

--- threader.py
+++ threader.py
@@ -1,3 +1,4 @@
+from posix import EX_NOPERM
 from pororo import Pororo
 from krwordrank.word import KRWordRank, summarize_with_keywords
 import operator
@@ -6,11 +7,18 @@ import os
 
 
 ### Get .txt files from directory /textfiles
+os.chdir('meeting_example')
+ex_text = []
+for f in os.listdir():
+    with open(f) as text_file:
+        text_data = text_file.read().replace("\n", " ")
+        text_file.close()
+        ex_text.append(text_data)
 
 ### Define functions for keyword threading
 compare_keylist = Pororo(task="zero-topic", lang="ko")
 keyword_list = []
-text_list = {}
+text_list = []
 
 textNum = 1
 
@@ -25,25 +33,24 @@ class TextClass:
 
 def keyword_threader(text, keyword_list):
     newText = TextClass(text)
-    text_list[textNum] = newText
-    textNum += 1
+    text_list.append(newText)
 
     keyScore = compare_keylist(text, keyword_list)
-    maxScore = max(keyScore, key=lambda key: keyScore[key])
+    maxKey = max(keyScore, key=lambda key: keyScore[key])
 
-    if maxScore >= 60:
+    if keyScore[maxKey] >= 50:
         newText = put_in_original_keyword(newText, keyScore)
         print("기존의 키워드에 포함됩니다: ", end="")
         print(newText.keywords)
     else:
+        newText = extract_new_keyword(newText)
         print("새로운 키워드가 추가되었습니다: ", end="")
         print(newText.keywords)
-        newText = extract_new_keyword(newText)
 
 
 def put_in_original_keyword(newText, keyScore):
     for key, value in keyScore.items():
-        if value >= 60:
+        if value >= 50:
             newText.add_keyword(key)
 
     return newText
@@ -59,17 +66,36 @@ def extract_new_keyword(newText):
         return newText
     except AttributeError:
         print("Attribute Error 발생")
-    
-    
-
-
-
-
-    return
-
 
 
 
 ### Get input for default keyword list
+if __name__ == "__main__":
+    while True:
+        input_keyword = input("Default thread를 입력해주세요 (건너뛰기: Enter): ")
+        if input_keyword == "":
+            break
+        else:
+            keyword_list.append(input_keyword)
+        
+    try: 
+        if len(keyword_list) == 0:
+            raise ValueError
+    except ValueError:
+        print("키워드를 1개 이상 입력해야 합니다.")
 
+    print("\nDefault Keyword List: ", end="")
+    print(keyword_list)
+    print()
 
+    stopwords = {}
+    j = 1
+    for text in ex_text:
+        print("Text%d: " % j + text[:50] + "...")
+        keyword_threader(text, keyword_list)
+        print()
+        j += 1
+    
+    print(keyword_list)
+
+    
