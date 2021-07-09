5b673333fa4c0b6f4ce7384e1d6a894ef156b88a
Yuseung Lee <phillip9807@gmail.com>
2021-07-10 01:25:07 +0900

    ADD: Noun extraction

--- threader.py
+++ threader.py
@@ -1,19 +1,27 @@
 from posix import EX_NOPERM
 from pororo import Pororo
 from krwordrank.word import KRWordRank, summarize_with_keywords
-import operator
-import json
 import os
+from konlpy.tag import Okt
 
+# Initialize Okt object
+okt = Okt()
 
 ### Get .txt files from directory /textfiles
 os.chdir('meeting_example')
 ex_text = []
-for f in os.listdir():
+list_of_files = sorted(os.listdir())
+for f in list_of_files:
     with open(f) as text_file:
         text_data = text_file.read().replace("\n", " ")
         text_file.close()
         ex_text.append(text_data)
+
+# for f in os.listdir():
+#     with open(f) as text_file:
+#         text_data = text_file.read().replace("\n", " ")
+#         text_file.close()
+#         ex_text.append(text_data)
 
 ### Define functions for keyword threading
 compare_keylist = Pororo(task="zero-topic", lang="ko")
@@ -38,7 +46,7 @@ def keyword_threader(text, keyword_list):
     keyScore = compare_keylist(text, keyword_list)
     maxKey = max(keyScore, key=lambda key: keyScore[key])
 
-    if keyScore[maxKey] >= 50:
+    if keyScore[maxKey] >= 70:
         newText = put_in_original_keyword(newText, keyScore)
         print("기존의 키워드에 포함됩니다: ", end="")
         print(newText.keywords)
@@ -50,24 +58,29 @@ def keyword_threader(text, keyword_list):
 
 def put_in_original_keyword(newText, keyScore):
     for key, value in keyScore.items():
-        if value >= 50:
+        if value >= 70:
             newText.add_keyword(key)
-
     return newText
 
 def extract_new_keyword(newText):
     text = newText.text
     sentences = text.split('. ')
+    noun_list = okt.nouns(text)         # Get list of nouns in the text
+
     try:
         keywords = summarize_with_keywords(sentences, min_count=1, max_length=15)
-        for word, r in sorted(keywords.items(), key=lambda x:x[1], reverse=True)[:3]:
-            keyword_list.append(word)
-            newText.add_keyword(word)
+        # Choose top 3 'noun' keywords
+        num = 0
+        for word, r in sorted(keywords.items(), key=lambda x:x[1], reverse=True):
+            if num == 3:
+                break
+            if word in noun_list:
+                keyword_list.append(word)
+                newText.add_keyword(word)
+                num += 1
         return newText
     except AttributeError:
         print("Attribute Error 발생")
-
-
 
 ### Get input for default keyword list
 if __name__ == "__main__":
